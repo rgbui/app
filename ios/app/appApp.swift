3786610ae68246a9755b7c4b171ac060f0a8e368
kanhai <760489033@qq.com>
2021-09-20 16:06:41 +0800

    ios

---
+++ ios/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by kanhai on 2021/9/20.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
